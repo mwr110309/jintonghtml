75530116cc7aea9316418798f7a0454c386ba90f
Nyya <799294923@qq.com>
2018-06-30 10:18:16 +0800

    group4
    constructor

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # jintong
 jintong
+
---
+++ js/iscroll.js
@@ -0,0 +1,11 @@
+$(function () {
+    let myScroll = new IScroll('.conIScroll',{
+        click:true,
+        scrollbars: true,
+        interactiveScrollbars: true,
+        shrinkScrollbars: 'scale',
+        fadeScrollbars: true,
+        probeType:3
+    })
+
+})
